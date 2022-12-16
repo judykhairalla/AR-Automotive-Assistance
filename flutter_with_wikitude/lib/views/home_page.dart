e23bfbb5d3926e3d0276fb544b59bf65bd3fe2df
rana-raafat <ranaraafat7801@gmail.com>
2022-12-17 00:49:18 +0200

    final home page

--- flutter_with_wikitude/lib/views/home_page.dart
+++ flutter_with_wikitude/lib/views/home_page.dart
@@ -16,47 +16,93 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: SafeArea(
+        left: true,
         child: Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image(
               image: const AssetImage("assets/images/car.png"),
-              alignment: Alignment.centerRight,
+              alignment: Alignment.centerLeft,
               height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width / 2,
             ),
-            Column(
-              children: [
-                //     Expanded(
-                //       child: Container(
-                //         color: Colors.amber,
-                //         child: Text('Above'),
-                //       ),
-                //     ),
-                //     const Divider(
-                //       height: 20,
-                //       thickness: 5,
-                //       indent: 20,
-                //       endIndent: 0,
-                //       color: Colors.black,
-                //     ),
-                ElevatedButton(
-                  child: Row(
-                    children: [
-                      const Text("Start"),
-                      // Icon(Image.asset(
-                      //   "assets/images/car.png",
-                      //   color: Colors.grey,
-                      // )),
-                    ],
-                  ),
-                  onPressed: () {
-                    GoRouter.of(context).push('/ar/intro_page');
-                  },
+            Container(
+              alignment: Alignment.centerLeft,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: MediaQuery.of(context).size.width / 15,
                 ),
-              ],
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: ListTile.divideTiles(context: context, tiles: [
+                      CustomListTile(
+                          context, "coolant", "Add Coolant", "/ar/intro_page"),
+                      CustomListTile(context, "tyre", "Change Tyre", null),
+                      CustomListTile(
+                          context, "car_battery", "Jumpstarting", null),
+                      CustomListTile(
+                          context, "light", "Change Headlights", null),
+                      CustomListTile(
+                          context, "windshield", "Replace Wipers", null),
+                    ]).toList(),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+Widget CustomListTile(
+    BuildContext context, String icon, String text, String? path) {
+  return Container(
+    height: MediaQuery.of(context).size.height / 7,
+    padding: EdgeInsets.all(MediaQuery.of(context).size.height / 57),
+    child: ListTile(
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ImageIcon(
+            AssetImage("assets/icons/$icon.png"),
+            color: Colors.black87,
+            size: MediaQuery.of(context).size.height / 15,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.height / 25,
+                right: MediaQuery.of(context).size.height / 25),
+            child: Text(text,
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.height / 25,
+                )),
+          ),
+        ],
+      ),
+      trailing: SizedBox(
+        height: MediaQuery.of(context).size.height / 19,
+        width: MediaQuery.of(context).size.width / 15,
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                backgroundColor: Colors.black87),
+            onPressed: () {
+              if (path == null) {
+              } else {
+                GoRouter.of(context).push(path);
+              }
+            },
+            child: Icon(
+              Icons.arrow_forward,
+              size: MediaQuery.of(context).size.height / 21,
+            )),
+      ),
+    ),
+  );
 }
